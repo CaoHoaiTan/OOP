6389708815a6fa4b09b2449e13b68c97516c5a15
CaoHoaiTan <70707794+CaoHoaiTan@users.noreply.github.com>
2020-12-29 20:45:11 +0700

    Update Program.cs

--- DoAnCuoiKi/QuanLiKhachSan/QuanLiKhachSan/Program.cs
+++ DoAnCuoiKi/QuanLiKhachSan/QuanLiKhachSan/Program.cs
@@ -10,6 +10,34 @@ namespace QuanLiKhachSan
     {
         public static List<KhachHang> dsKHACHHANG =
             new List<KhachHang>();
+        public static List<LoaiKhachHang> dsLoaiKhachHang =
+            new List<LoaiKhachHang>();
+        public static List<CongViec> dsCongViec =
+            new List<CongViec>();
+        public static List<DichVu> dsDichVu =
+            new List<DichVu>();
+        public static List<HoaDon> dsHoaDon =
+            new List<HoaDon>();
+        public static List<HopDong> dsHopDong =
+            new List<HopDong>();
+        public static List<LAPHOPDONG> dsLHD =
+            new List<LAPHOPDONG>();
+        public static List<LoaiPhong> dsLoaiPhong =
+            new List<LoaiPhong>();
+        public static List<LoaiThietBi> dsLoaiThietBi =
+            new List<LoaiThietBi>();
+        public static List<NhanVien> dsNhanVien =
+            new List<NhanVien>();
+        public static List<NhomCongViec> dsNhomCongViec =
+            new List<NhomCongViec>();
+        public static List<NhomNhanVien> dsNhomNhanVien =
+            new List<NhomNhanVien>();
+        public static List<Phong> dsPhong =
+            new List<Phong>();
+        public static List<THAOTACDICHVU> dsTTDV =
+            new List<THAOTACDICHVU>();
+        public static List<ThietBi> dsThietBi =
+            new List<ThietBi>();
         static void Main(string[] args)
         {
         }
